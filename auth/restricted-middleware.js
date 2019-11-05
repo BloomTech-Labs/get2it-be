b6921354afc36aeb67d832b2a9f7f63c2bd85c68
Wayne Mitt <wmitt85@gmail.com>
2019-11-04 21:06:45 -0600

    register and login endpoints

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,18 @@
+const jwt= require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({message: 'not verified'})
+      } else {
+        req.decodedToken = decodedToken
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({message: 'no token provided'})
+  }
+};
