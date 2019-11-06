e84116b53acb54750745bb6829ce3448cdf6b0f7
Wayne Mitt <wmitt85@gmail.com>
2019-11-05 19:22:48 -0600

    added production config variables

--- knexfile.js
+++ knexfile.js
@@ -21,4 +21,15 @@ module.exports = {
         directory: './database/seeds',
       },
     },
+    production: {
+      client: 'mysql',
+      version: '2.17.1',
+      useNullAsDefault: true,
+      connection: {
+        host: "us-cdbr-iron-east-05.cleardb.net",
+        user: "b98d8364ee5a1c",
+        password: "7920ee1e",
+        database: "heroku_d1dd11759650ef7",
+      }
+    }
   };
