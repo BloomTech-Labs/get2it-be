d15a401dfbee0f078b6168e0939bf4db8d21a46e
Randy Warrington <warrington.randy@outlook.com>
2019-11-11 21:45:17 -0500

    changed label color to task icon in seed file

--- database/seeds/001-tasks.js
+++ database/seeds/001-tasks.js
@@ -11,21 +11,21 @@ exports.seed = function(knex) {
          date: 'November 11, 2019', 
          start_time: '10:00 pm', 
          end_time: '11:00 pm', 
-         label_color: 'blue' },
+         task_icon: 'book icon' },
         {user_id: 2,
          name: 'read two chapters of book', 
          status: 'completed',
          date: 'December 15, 2019',
          start_time: '5:20pm',
          end_time: '7:00 pm',
-         label_color: 'green' },
+         task_icon: 'book icon' },
         {user_id: 3,
          name: 'study 3 hours of code', 
          status: 'not completed',
          date: 'December 21, 2019',
          start_time: '2:00 pm',
          end_time: '5:00 pm',
-         label_color: 'red'  }
+         task_icon: 'computer icon'  }
       ]);
     });
 };
