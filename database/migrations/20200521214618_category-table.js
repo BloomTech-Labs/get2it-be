97ffb3c8048aff1e1a7c4860a504b45135bcfa99
Jay Leach <leachcoding@gmail.com>
2020-06-03 20:26:05 -0400

    Added task id to categories, reversed from what we had

--- database/migrations/20200521214618_category-table.js
+++ database/migrations/20200521214618_category-table.js
@@ -2,7 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('categories', categories => {
     categories.increments();
-    categories.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+    categories.integer('user_id').unsigned().notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('RESTRICT')
+    categories.integer('task_id').unsigned().notNullable().references('id').inTable('tasks').onUpdate('CASCADE').onDelete('RESTRICT');
     categories.string('name').notNullable();
   })
 };
