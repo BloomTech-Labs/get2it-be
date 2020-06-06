65abda5379e2cea026f37d123f80df77fd31a31e
Aaron Williams <aaronrw4@yahoo.com>
2020-06-05 20:54:33 -0700

    Added bcrypt for password in seeds

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,20 +1,22 @@
+const bcrypt = require('bcryptjs');
+
 exports.seed = function(knex) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
         {
           displayName: "bob1",
-          password: '1234test',
+          password: bcrypt.hashSync('1234test', 10),
           email: 'bob1@gmail.com'
         },
         {
           displayName: "bob2",
-          password: '1234test',
+          password: bcrypt.hashSync('1234test', 10),
           email: 'bob2@gmail.com'
         },
         {
           displayName: "bob3",
-          password: '1234test',
+          password: bcrypt.hashSync('1234test', 10),
           email: 'bob3@gmail.com'
         }
       ]);
