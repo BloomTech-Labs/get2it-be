741c10e301ae80b01c917aa0cc5236990a160062
Aaron Williams <aaronrw4@yahoo.com>
2020-06-16 15:07:04 -0700

    Changed task.add to return id correctly

--- users/users-router.js
+++ users/users-router.js
@@ -25,10 +25,9 @@ router.post('/:id/tasks', restricted, (req, res) => {
 
   Tasks.add(task)
     .then(newTask => {
-      console.log(newTask)
       res.status(201).json({
-        id: newTask[0],
-        message: 'Task created successfully'        
+        message: 'Task created successfully',
+        id: newTask.id        
       });
     })
     .catch(err => {
